fcff2f9ac820f706a61a792c87a9b5b9ea0f0c2e
kshitij <kshitijkant@example.com>
2019-07-31 22:50:47 +0530

    first commit

---
+++ treesNGraphs/binaryTree.py
@@ -0,0 +1,3 @@
+# implment Node class to be used by tree class
+
+# implement a BST class ,supporting instantiation with a root node, insertion, deletion and search methods.
---
+++ treesNGraphs/graphClass.py
@@ -0,0 +1,9 @@
+#implement Node class to be used by graph class
+
+# class Node():
+
+
+
+
+# #instantiate a graph from a adjacency matrix
+# class graph():
