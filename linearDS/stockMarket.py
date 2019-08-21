f4c6217a7a39c6e5ecfbe17ba5efcb1e8e61145c
kshitij <kshitijkant@example.com>
2019-08-21 10:16:34 +0530

    added problem

---
+++ linearDS/stockMarket.py
@@ -0,0 +1,10 @@
+# Find the Best Time To Buy And Sell Stock
+# Say you have an array for which the ith element is the price of a given stock on day i.
+# If you were only permitted to buy one share of the stock and sell one share of the
+# stock, design an algorithm to find the best times to buy and sell.
+#
+# Ex:
+# Input = {6,1,3,2,4,7}
+# Output : time to buy = at index 1(value = 1)
+#              time to sell  = at index 5(value = 7)
+#              max profit   = 7 - 1 = 6
